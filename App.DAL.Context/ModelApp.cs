4ffaddf93e39594fb51e2ea9aadea72709217245
ESSARRAJ Fouad <essarraj.fouad@gmail.com>
2019-03-04 13:29:49 +0100

    Save

---
+++ App.DAL.Context/ModelApp.cs
@@ -0,0 +1,23 @@
+﻿namespace App.DAL.Context
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ModelApp : DbContext
+    {
+        public ModelApp() : base(GetConnectionString())
+        {
+            // @"data source=(LocalDb)\MSSQLLocalDB;initial catalog=TrainingIS;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"
+        }
+
+        public static string GetConnectionString()
+        {
+
+            string ConnectionString = @"data source=(LocalDb)\MSSQLLocalDB;initial catalog=Main_App;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+            return ConnectionString;
+        }
+
+    }
+}
